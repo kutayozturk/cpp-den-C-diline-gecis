5d41672cc3f60f1b9a1b3bc6f2945477f6f3279e
Kutay ÖZTÜRK <94574681+kutayozturk@users.noreply.github.com>
2022-12-16 16:38:02 +0300

    Add files via upload

---
+++ 01 - ilk c ornegi.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+// Yukarıda yer alan kod parçalarını inceleyiniz.
+
+/* Ekran çıktısında ondalıklı bir sayının kaç basamak ondalık göstereceğini belirlemek için aşağıdaki yöntem kullanılır.
+
+float a = 5.0;
+
+printf("%.1f",a); // 5.0
+printf("%.2f",a); // 5.00
+printf("%.3f",a); // 5.000
+printf("%.4f",a); // 5.0000
+printf("%.5f",a); // 5.00000
+printf("%.6f",a); // 5.000000
+
+*/
---
+++ 02 - char kullanımı.c
@@ -0,0 +1,10 @@
+// C dilinde string ifadeler
+
+/* C programlama dilinde string veri tipi olmadığı için, string veri tipi yerine char dizisi kullanılır.
+*/
+
+char isim[20]; // 20 elemanlı, char veri tipinde bir boyutlu dizi
+
+char *s; // s adinda bir pointer
+
+/* Yukarıda yer alan her iki durumda TEMELDE aynı anlama gelirler, *s ifadesindeki pointer, git s adında bir pointer oluştur ve içeresini bunları yaz demektir. */
